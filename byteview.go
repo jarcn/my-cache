f77a9ea703e71ffe22bb9e0a47aa2f5ead1dd047
chenjia <chenjia@qiyee-tech.com>
2022-03-09 14:18:48 +0800

    update

---
+++ byteview.go
@@ -0,0 +1,24 @@
+package mycache
+
+type ByteView struct {
+	b []byte //只读,防止缓存值被外部程序修改
+}
+
+// 实现Value接口,则是Value的子类
+func (v ByteView) Len() int {
+	return len(v.b)
+}
+
+func (v ByteView) ByteSlice() []byte {
+	return cloneBytes(v.b)
+}
+
+func cloneBytes(b []byte) []byte {
+	c := make([]byte, len(b))
+	copy(c, b)
+	return c
+}
+
+func (v ByteView) String() string {
+	return string(v.b)
+}
---
+++ mycache.go
@@ -0,0 +1,90 @@
+package mycache
+
+import (
+	"fmt"
+	"log"
+	"sync"
+)
+
+type Getter interface {
+	Get(key string) ([]byte, error)
+}
+
+//用户回调函数
+type GetterFunc func(key string) ([]byte, error)
+
+//缓存中没有,具体从哪个数据源查询数据,由用户自己决定
+func (f GetterFunc) Get(key string) ([]byte, error) {
+	return f(key)
+}
+
+//核心数据结构
+type Group struct {
+	name      string
+	getter    Getter
+	mainCache cache
+}
+
+var (
+	mu     sync.RWMutex
+	groups = make(map[string]*Group)
+)
+
+//创建核心数据结构
+func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
+	if getter == nil {
+		panic("nil Getter")
+	}
+	mu.Lock()
+	defer mu.Unlock()
+	g := &Group{
+		name:      name,
+		getter:    getter,
+		mainCache: cache{cacheBytes: cacheBytes},
+	}
+	groups[name] = g
+	return g
+}
+
+func GetGroup(name string) *Group {
+	mu.Lock()
+	defer mu.Unlock()
+	g := groups[name]
+	return g
+}
+
+func (g *Group) Get(key string) (ByteView, error) {
+	if key == "" {
+		return ByteView{}, fmt.Errorf("key is required")
+	}
+	if v, ok := g.mainCache.get(key); ok {
+		log.Println("[MyCache hit]")
+		return v, nil
+	}
+	return g.load(key)
+}
+
+func (g *Group) load(key string) (ByteView, error) {
+	bytes, err := g.getter.Get(key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	value := ByteView{b: cloneBytes(bytes)}
+	g.populateCache(key, value)
+	return value, nil
+}
+
+func (g *Group) getLocally(key string) (ByteView, error) {
+	bytes, err := g.getter.Get(key)
+	if err != nil {
+		return ByteView{}, err
+
+	}
+	value := ByteView{b: cloneBytes(bytes)}
+	g.populateCache(key, value)
+	return value, nil
+}
+
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.add(key, value)
+}
